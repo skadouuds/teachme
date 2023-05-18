1a0bb0e1ddc6be7469c20ed6b738d9c72c7f656e
Bella <annabellaputridirgo@gapp.nthu.edu.tw>
2023-05-18 12:02:35 +0800

    Modify: complete landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Jumbotron from "../components/Jumbotron";
+import About from "../components/About";
+import Howtouse from "@/components/Howtouse";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Jumbotron />
+      <About />
+      <Howtouse />
     </>
   );
 }
